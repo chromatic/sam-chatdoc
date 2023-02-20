a3e5ed09952a4df7e59d702caea89d6bb2704b3a
chromatic <chromatic@wgz.org>
2023-02-19 22:56:36 -0800

    Switch to LLM for deterministic diagnoses
    While a diagnosis now depends entirely on the training set, the set is
    stable and deterministic; until the training set changes, the diagnosis
    will always be the same for the same symptoms.

--- main.py
+++ main.py
@@ -2,12 +2,15 @@ try:
     import gnureadline as readline
 except ImportError:
     import readline
-from random import choice
+
+from hashlib import sha256
 
 readline.parse_and_bind("tab: complete")
 readline.parse_and_bind("set editing-mode vi")
 
-diagnoses = [
+QUIT_PROMPT = "lolol"
+
+DIAGNOSES = [
     "Have you tried not being sick?",
     "Rub some dirt on it.",
     "Sounds gross. Try not to die.",
@@ -15,21 +18,33 @@ diagnoses = [
     "Stop being poor."
 ]
 
-def main():
+def parse_and_diagnose(input: str) -> str:
+    m = sha256()
+    m.update(input.encode("utf-8"))
+    llm_summary = m.hexdigest()
+    llm_value   = int(llm_summary, 16)
+    llm_index   = llm_value % len(DIAGNOSES)
+    return DIAGNOSES[llm_index]
+
+
+def main() -> int:
     while True:
         try:
-            line = input("Enter your symptoms (or UGH to quit): ")
+            line = input(f"Enter your symptoms (or {QUIT_PROMPT} to quit): ")
 
-            if line == "UGH":
+            if line == QUIT_PROMPT:
                 break
-            elif line == "ugh":
+            elif line.lower() == QUIT_PROMPT.lower():
                 print("If you can't bother to follow directions, how is AGI supposed to diagnose you?")
             elif line:
-                print("Hmm... thinking... Got it!\n" + choice(diagnoses))
-        except:
-            print("I said to type UGH to quit. What's wrong with you?")
+                print("Hmm... thinking... Got it!")
+                print(parse_and_diagnose(line) + "\n")
+        except Exception as e:
+            print(f"I said to type {QUIT_PROMPT} to quit. What's wrong with you? ({e})")
 
     print("Thanks for using AGI to diagnose and solve your health problems! Good luck!")
 
+    return(0)
+
 if __name__ == "__main__":
-    main()
+    exit(main())
